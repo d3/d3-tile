0af5cf8d03bc917cbe68c5ee59297fa904de9e76
Chanwut Kittivorawong <chanwutk@gmail.com>
2021-05-11 14:06:03 -0700

    strict typing for scale and translate

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,37 +1,58 @@
-type Tile = {
-  (...args: any): [number, number, number][] & {
+type BaseTile<ScaleArgs extends any[], TranslateArgs extends any[]> = {
+  size(): [number, number];
+  extent(): [[number, number], [number, number]];
+  scale(): (...args: ScaleArgs) => number;
+  translate(): (...args: TranslateArgs) => [number, number];
+  zoomDelta(): () => number;
+  tileSize(): number;
+  clamp(): boolean;
+  clampX(): boolean;
+  clampY(): boolean;
+};
+
+type ScaleMethodsMixins<TranslateArgs extends any[]> = {
+  scale(params: () => number): ConstantScaleTile<TranslateArgs>;
+  scale(params: (...args: TranslateArgs) => number): Tile<TranslateArgs>;
+  scale(params: null | number): ConstantScaleTile<TranslateArgs>;
+  scale<InvalidScaleArgs extends any[]>(params: (...args: InvalidScaleArgs) => number): InvalidTile<InvalidScaleArgs, TranslateArgs>;
+}
+
+type TranslateMethodsMixins<ScaleArgs extends any[]> = {
+  translate(params: () => [number, number]): ConstantTranslateTile<ScaleArgs>;
+  translate(params: (...args: ScaleArgs) => [number, number]): Tile<ScaleArgs>;
+  translate(params: null | [number, number]): ConstantTranslateTile<ScaleArgs>;
+  translate<InvalidTranslateArgs extends any[]>(params: (...args: InvalidTranslateArgs) => [number, number]): InvalidTile<ScaleArgs, InvalidTranslateArgs>;
+}
+
+type InvalidTile<ScaleArgs extends any[], TranslateArgs extends any[]> = BaseTile<ScaleArgs, TranslateArgs> & ScaleMethodsMixins<TranslateArgs> & TranslateMethodsMixins<ScaleArgs>;
+
+type ConstantScaleTile<TranslateArgs extends any[]> = BaseTile<[], TranslateArgs> & ScaleMethodsMixins<TranslateArgs> & {
+  translate(params: () => [number, number]): Tile<[]>;
+  translate(params: null | [number, number]): Tile<[]>;
+  translate<InvalidTranslateArgs extends any[]>(params: (...args: InvalidTranslateArgs) => [number, number]): ConstantScaleTile<InvalidTranslateArgs>;
+}
+
+type ConstantTranslateTile<ScaleArgs extends any[]> = BaseTile<ScaleArgs, []> & TranslateMethodsMixins<ScaleArgs> & {
+  scale(params: () => number): Tile<[]>;
+  scale(params: null | number): Tile<[]>;
+  scale<InvalidScaleArgs extends any[]>(params: (...args: InvalidScaleArgs) => number): ConstantTranslateTile<InvalidScaleArgs>;
+};
+
+type Tile<Args extends any[]> = InvalidTile<Args, Args> & {
+  (...args: Args): [number, number, number][] & {
     translate: [number, number];
     scale: number;
   };
 
-  size(param: null | [number, number]): Tile;
-  size(): [number, number];
-
-  extent(param: null | [[number, number], [number, number]]): Tile;
-  extent(): [[number, number], [number, number]];
-
-  scale(params: (() => number) | null | number): Tile;
-  scale(): () => number;
-
-  translate(params: (() => [number, number]) | null | [number, number]): Tile;
-  translate(): () => [number, number];
-
-  zoomDelta(params: null | number): Tile;
-  zoomDelta(): () => number;
-
-  tileSize(params: null | number): Tile;
-  tileSize(): number;
-
-  clamp(params: null | boolean): Tile;
-  clamp(): boolean;
-
-  clampX(params: null | boolean): Tile;
-  clampX(): boolean;
-
-  clampY(params: null | boolean): Tile;
-  clampY(): boolean;
+  size(param: null | [number, number]): Tile<Args>;
+  extent(param: null | [[number, number], [number, number]]): Tile<Args>;
+  zoomDelta(params: null | number): Tile<Args>;
+  tileSize(params: null | number): Tile<Args>;
+  clamp(params: null | boolean): Tile<Args>;
+  clampX(params: null | boolean): Tile<Args>;
+  clampY(params: null | boolean): Tile<Args>;
 }
 
-export function tile(): Tile;
+export function tile(): Tile<[{ x: number, y: number, k: number }]>;
 
 export function tileWrap(param: [number, number, number]): [number, number, number];
